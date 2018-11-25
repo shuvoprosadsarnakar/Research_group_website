71a709a7db56fe586ecb8a9f2493744bea647afa
pusku <puskuuuu@gmail.com>
2018-11-25 16:12:35 +0600

    Implemented Crud of Group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use Session;
+use File;
+use DB;
+use App\Http\Requests;
+use App\Member;
+use App\Group;
+use App\MemberGroup;
 
 class GroupController extends Controller
 {
@@ -11,75 +19,47 @@ class GroupController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    /*
+    public function __construct()
     {
-        //
+        $this->middleware('auth');
+    }
+    */
+    public function addGroup(){
+        $data['data'] = DB::table('groups')->get();
+        return view('memberCreateOrEdit',$data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        
+    public function insertGroup(Request $request){
+        $this->validate($request,[
+            'groupName'=>'required|max:255',
+        ]);
+        $groupName = $request->input('groupName');
+        $data=array('groupName'=>$groupName);
+        DB::table('groups')->insert($data);
+        $request->session()->flash('alert-success', 'Group was successful added!');
+        return redirect()->route("member_create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function editGroup($id) {
+        $data['data'] = DB::table('groups')->get();
+        $memberEditInfo = Member::find($id);
+        return view('memberCreateOrEdit',$data,$memberEditInfo);
+    }
+    public function deleteGroup($id){
+        $data=Group::find($id);
+        Group::destroy($id);
+        return redirect()->route("addGroup")->with('flash_message', 'Group deleted!');
+    }
+    public function updateGroup(Request $request, $id){
+       
+        $groupName = $request->input('groupName');
+        $data=array('groupName'=>$groupName);
+        Member::where('id',$id)->update($data);
+        $request->session()->flash('alert-success', 'Group was successful Updated!');
+        return redirect()->route("member_create");
+        }
+
         
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
