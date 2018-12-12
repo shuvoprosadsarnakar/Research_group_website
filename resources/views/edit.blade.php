0bb20c212f0fa719d7df4f5e0d708954c40ea939
pusku <puskuuuu@gmail.com>
2018-12-13 03:07:20 +0600

    fixing design

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,45 +1,45 @@
 <div class="container">
     <div class="well row">
 
-        <div class="col-md-1">
+        <div class="col-sm-1">
             <a href="{{ route('post_create',['criteria' => 'startDate','order' => 'desc']) }}">
                 <h4>Posts</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
 
             <a href="{{ route('member_create') }}">
                 <h4>Members</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
 
             <a href="{{ route('type_create') }}">
                 <h4>Types</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
 
             <a href="{{ route('image_create') }}">
                 <h4>Images</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
             <a href="{{ route('video_create') }}">
                 <h4>Videos</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
             <a href="{{ route('group_create') }}">
                 <h4>Groups</h4>
             </a>
         </div>
-        <div class="col-md-1">
+        <div class="col-sm-1">
             <a href="{{ route('report_create') }}">
                 <h4>Reports</h4>
             </a>
         </div>
-        <div class="col-md-2">
+        <div class="col-sm-2">
             <a href="{{ route('publication_create') }}">
                 <h4>Publications</h4>
             </a>
